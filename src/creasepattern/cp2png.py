1e101ea52333c6eca96457c87da26cce4402cf13
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-08-06 23:12:04 +0200

    Improve cp2png by forcing to scale within a specific box

--- src/creasepattern/cp2png.py
+++ src/creasepattern/cp2png.py
@@ -1,32 +1,38 @@
+from sys import maxsize
 from .cp import Cp
+import math
 
 from PIL import Image, ImageDraw
 
-factor = 8
 
-margin = 20
-
-def cp2png(infile, outfile):
+def cp2png(infile, outfile, size=2048, margin=20, aa_scale=4):
     cp = Cp(infile)
 
     width, height = cp.size()
 
-    with Image.new('RGB', (width * factor + 2 * margin, height * factor + 2 * margin)) as im:
+    if width < height:
+        factor = size * aa_scale / height
+    else:
+        factor = size * aa_scale / width
+
+    if width < height:
+        innerFactor = (size - margin) * aa_scale / (height)
+    else:
+        innerFactor = (size - margin) * aa_scale / (width)
+
+    with Image.new('RGB', (math.ceil(width * factor), math.ceil(height * factor))) as im:
         draw = ImageDraw.Draw(im)
 
         # Fill background
-        draw.rectangle([0, 0, width * factor + 2 * margin, height * factor + 2 * margin],
+        draw.rectangle([0, 0, width * factor, height * factor],
                        fill=(255, 255, 255))
 
         for line in cp.lines:
-            draw.line((((line.x1 - cp.minX) * factor) + margin, ((line.y1 - cp.minY) * factor) + margin,
-                       ((line.x2 - cp.minX) * factor) + margin, ((line.y2 - cp.minY) * factor) + margin), fill=line.color(), width=2)
+            draw.line((((line.x1 - cp.minX) * innerFactor) + margin, ((line.y1 - cp.minY) * innerFactor) + margin,
+                       ((line.x2 - cp.minX) * innerFactor) + margin, ((line.y2 - cp.minY) * innerFactor) + margin), fill=line.color(), width=aa_scale)
 
         # Apply anti alias
         im = im.resize(
-            ((width * factor + (2 * margin)) // 2, (height * factor + (2 * margin)) // 2), resample=Image.ANTIALIAS)
+            (math.ceil((width * factor) // aa_scale), math.ceil((height * factor) // aa_scale)), resample=Image.ANTIALIAS)
 
         im.save(outfile, "PNG")
-
-if __name__ == '__main__':
-    cp2png('snek2.cp', 'snek2.png')
