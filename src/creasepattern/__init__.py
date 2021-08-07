1171035c95a47661e5fb0aebe0acbb400c03cce1
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-08-07 17:13:56 +0200

    Add from_orh to exports

--- src/creasepattern/__init__.py
+++ src/creasepattern/__init__.py
@@ -6,6 +6,7 @@ from .orh2png import *
 
 from .from_cp import from_cp, from_cp_str
 from .from_opx import from_opx
+from .from_orh import from_orh
 
 from .to_png import to_png
 from .to_svg import to_svg
